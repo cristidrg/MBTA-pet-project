28a5e423b75e2a9897f3e5a2cdd6af9a46e7504e
Provost Digital <pods@Provosts-MacBook-Pro-2.local>
2019-06-19 22:19:11 -0400

    Add constants, format files

--- components/Stations.vue
+++ components/Stations.vue
@@ -1,87 +1,91 @@
 <script>
-import SelectComponent from "vue-select"
-import 'vue-select/dist/vue-select.css';
+import SelectComponent from "vue-select";
+import "vue-select/dist/vue-select.css";
 
 export default {
-    components: {
-        SelectComponent
-    }
-}
-
+  components: {
+    SelectComponent
+  }
+};
 </script>
 
 <template>
   <section class="container">
-      <p class="title">{{ $t('stations.title') }}</p>
-      <div class="filters">
-        <div class="line" />
-        <select-component
-            @input="e => e" 
-            class="filters__station"
-            value="ALL"
-            :clearable="false"
-            :searchable="false"
-         />
-        <div class="line" />
-      </div>
+    <p class="title">{{ $t('stations.title') }}</p>
+    <div class="filters">
+      <div class="line"/>
+
+      <select-component
+        @input="e => e"
+        class="filters__station"
+        value="ALL"
+        :clearable="false"
+        :searchable="false"
+      />
+      <div class="line"/>
+    </div>
+
+    <ul>
+        <li class="" v-for="item in items">{{ }}</li>
+    </ul>
   </section>
 </template>
 <style lang="scss" scoped>
-    .container {
-        background-color: $white;
-        border-radius: 15px 15px 0px 0px;
-        width: 100%;
-        margin-top: 15px;
-    }
+.container {
+  background-color: $white;
+  border-radius: 15px 15px 0px 0px;
+  width: 100%;
+  margin-top: 15px;
+}
 
-    .title {
-        @include header;
+.title {
+  @include header;
 
-        text-transform: uppercase;
-        text-align: center;
-        padding: 15px;       
-    }
+  text-transform: uppercase;
+  text-align: center;
+  padding: 15px;
+}
 
-    .filters {
-        display: flex;
-        align-items: center;
+.filters {
+  display: flex;
+  align-items: center;
 
-        &__station {
-            @include header-2;
-            min-width: 300px;
-        }
-    }
-    
-    .line {
-        height: 4px;
-        display: block;
-        background-color: $gray01;
-        width: 100%;
-    }
+  &__station {
+    @include header-2;
+    min-width: 300px;
+  }
+}
+
+.line {
+  height: 4px;
+  display: block;
+  background-color: $gray01;
+  width: 100%;
+}
 </style>
 
 <!-- Overrides for the plugin -->
 <style lang="scss">
-    .filters {
-        .vs__dropdown-toggle {
-            border: none;
-        }
+.filters {
+  .vs__dropdown-toggle {
+    border: none;
+  }
 
-        .vs__selected-options {
-            justify-content: center;
-            padding-left: 20px;
-        }
+  .vs__selected-options {
+    justify-content: center;
+    padding-left: 20px;
+  }
 
-        .vs__selected-options > .vs__search {
-            position: absolute;
-        }
+  .vs__selected-options > .vs__search {
+    position: absolute;
+  }
 
-        .vs__selected-options > .vs__search[aria-expanded=true] {
-            position: static;
-        }
+  .vs__selected-options > .vs__search[aria-expanded="true"] {
+    position: static;
+  }
 
-        .vs__actions {
-            padding-right: 20px;
-        }
-    }
+  .vs__actions {
+    padding-right: 20px;
+  }
+}
 </style>
--- store/index.js
+++ store/index.js
@@ -4,11 +4,26 @@ export const state = () => ({
     locale: 'en'
 });
 
+export const MBTA_LINES = {
+    '1': {
+        id: '1',
+        name: 'orange'
+    },
+    '2': {
+        id: '2',
+        name: 'red'
+    },
+    '3': {
+        id: '3',
+        name: 'blue'
+    }
+};
+
 export const mutations = {
     initStops (state, payload) {
         state.stops = payload
     },
-    SET_LANG (state, locale) {
+    set_lang (state, locale) {
         if (state.locales.indexOf(locale) !== -1) {
             state.locale = locale
         }
@@ -17,11 +32,10 @@ export const mutations = {
 
 export const actions = {
     nuxtServerInit({ commit }, { app }) {
-        return Promise.all([
-            app.mbta.fetchStopsByRoute('Orange').then(stop => stop.map(mapResponseToStore('Orange'))),
-            app.mbta.fetchStopsByRoute('Red').then(stop => stop.map(mapResponseToStore('Red'))),
-            app.mbta.fetchStopsByRoute('Blue').then(stop => stop.map(mapResponseToStore('Blue')))
-        ]).then(values => {
+        return Promise.all(Object.values(MBTA_LINES)
+            .map( ({ name }) => app.mbta.fetchStopsByRoute(name)
+                .then(stop => stop.map(mapResponseToStore(name))))
+        ).then(values => {
             commit('initStops', values.flat())
         })
     }
